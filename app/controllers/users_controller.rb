50b258433174765fe1c119197d69dd8e4248f992
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-17 16:39:53 +0100

    routes & user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def strong_params
+
+  end
+end
