f46f789585b802e001243bd19a34d393da96c7cd
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-17 17:16:36 +0100

    add new columns to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+fernando = User.create(name: 'Fernando Requeni', email: 'ferrenac@icloud.com', password: '123456789', chef: false)
