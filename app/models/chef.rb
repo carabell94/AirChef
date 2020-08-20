1b62a68ba672ed97178ed06837742c78757a3cf1
Cara Bell <carabell1994@aim.com>
2020-08-20 18:46:55 +0100

    review submitted added, total bookings added - all on profile page

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -12,6 +12,7 @@ include PgSearch::Model
 
   validates :name, :bio, :location, :years_experience, :hourly_rate, :cuisines, presence: true
   validates :years_experience, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 70}
+  validates :hourly_rate, numericality: { greater_than: 0 }
   validates :bio, length: { minimum: 10 }
 
   has_one_attached :photo
