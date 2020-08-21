592df20ed28b308909a820d502f34c8e76e680a3
Cara Bell <carabell1994@aim.com>
2020-08-21 15:55:46 +0100

    javascript fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,13 +28,13 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initStarRating } from '../plugins/init_star_rating';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initStarRating();
 });
 
-import { initStarRating } from '../plugins/init_star_rating';
 
-initStarRating();
