403508eed843e518afdffeda2606a0865bbf31b2
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-08-21 12:03:31 +0100

    update seeds with nickname

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,21 @@ require 'faker'
 cuisines = %w(Mexican Italian Indian Cajun Soul Thai Greek Chinese Lebanese Japanese American Moroccan Mediterranean French Spanish German Korean Vietnamese Turkish Caribbean)
 london_areas = ['Isle of Dogs', 'Belgravia', 'Blackheath', 'East Finchley', 'Nothing Hill', 'Fulham', 'Richmond', 'Kensington', 'Chelsea', 'Streatham', 'Hammersmith', 'Herne Hill', 'Hampstead', 'Islington', 'Clapham', 'Wandsworth', 'Bethnal Green', 'Crystal Palace', 'Camden', 'Hackney', 'Bermondsey', 'Brixton', 'Peckham', 'Leyton']
 
-cara_user = User.create!(email: "cara@gmail.com", password: "123456")
+cara_user = User.create!(nickname: 'Cara', email: "cara@gmail.com", password: "123456")
 
-lu_user = User.create!(email: "lu@gmail.com", password: "123456")
+lu_user = User.create!(nickname: 'Lu', email: "lu@gmail.com", password: "123456")
 
-shaun_user = User.create!(email: "shaun@gmail.com", password: "123456")
+shaun_user = User.create!(nickname: 'Shaun', email: "shaun@gmail.com", password: "123456")
 
-fernando_user = User.create!(email: "fernando@gmail.com", password: "123456")
+fernando_user = User.create!(nickname: 'Fer', email: "fernando@gmail.com", password: "123456")
 
-10.times do
-  user = User.create(
-    email: Faker::Internet.email,
-    password: '123456'
-  )
-  user.save!
-end
+# 10.times do
+#   user = User.create(
+#     email: Faker::Internet.email,
+#     password: '123456'
+#   )
+#   user.save!
+# end
 
 julio = Chef.create!(name: "Julio",  bio: "Julio's cooking is as great as his moustache. He is a whizz in the kitchen, specialising in Spanish-themed feasts.", years_experience: 10, hourly_rate: 10, location: "Mayfair, London", cuisines: "Spanish, Mexican, European" )
 
@@ -57,27 +57,27 @@ fernando = Chef.create!(name: "Fernando",  bio: 'Unbelievably skilled in the kit
 
 shaun = Chef.create!(name: "Shaun",  bio: 'Unbelievably skilled in the kitchen.', years_experience: 9, hourly_rate: 22, location: "Clerkenwell, London", cuisines: "British, Seafood, Chinese")
 
-20.times do
-  chef = Chef.create(
-    name: Faker::Name.first_name,
-    bio: Faker::Lorem.words(number: rand(4..30)).join(' '),
-    years_experience: rand(1..30),
-    hourly_rate: rand(10..100),
-    location: "#{london_areas.sample}, London",
-    cuisines: cuisines.sample(rand(1..6)).join(', ')
-    )
-  p chef.cuisines
-  chef.save!
-end
+# 20.times do
+#   chef = Chef.create(
+#     name: Faker::Name.first_name,
+#     bio: Faker::Lorem.words(number: rand(4..30)).join(' '),
+#     years_experience: rand(1..30),
+#     hourly_rate: rand(10..100),
+#     location: "#{london_areas.sample}, London",
+#     cuisines: cuisines.sample(rand(1..6)).join(', ')
+#     )
+#   p chef.cuisines
+#   chef.save!
+# end
 
 50.times do
   booking = Booking.create(
-    user: User.find_by_id(rand(1..14)),
+    user: User.find_by_id(rand(1..4)),
     date: Faker::Date.between(from: '2020-08-01', to: '2020-08-19'),
     duration: rand(5..10),
     guests: rand(2..50),
     location: london_areas.sample,
-    chef: Chef.find_by_id(rand(1..35)),
+    chef: Chef.find_by_id(rand(1..15)),
     cuisine: cuisines.sample
     )
   booking.save!
