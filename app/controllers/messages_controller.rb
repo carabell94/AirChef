60e8e5d3ef72ef898e720d7639d453840aa9b5fa
Cara Bell <carabell1994@aim.com>
2020-11-15 13:13:50 +0000

    added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,15 @@
 class MessagesController < ApplicationController
- 
+
   # def show
   #   @message = Message.find(params[:id])
   # end
 
-  def new
-    @message = Message.new
-  end
+  # def new
+  #   @message = Message.new
+  # end
 
   def create
-    @message = Message.new(strong_params)
+    @message = Message.new(message_params)
     if @message.save!
       redirect_to message_path(@message)
     else
@@ -19,7 +19,7 @@ class MessagesController < ApplicationController
 
   private
 
-  def strong_params
-    params.require(:message).permit(:user_id, :booking_id, :content)
+  def message_params
+    params.require(:message).permit(:user_id, :content)
   end
 end
